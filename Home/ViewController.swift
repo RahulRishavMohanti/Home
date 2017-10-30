1095a9591bfa60d5f6cf763fa4641ef2cb73224f
Rahul Mohanti <jarvis3.0@Rahuls-MacBook-Air.local>
2017-10-30 23:32:32 +0530

    Initial Commit

---
+++ Home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Home
+//
+//  Created by Rahul Rishav Mohanti on 30/10/17.
+//  Copyright © 2017 Rahul Rishav Mohanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
